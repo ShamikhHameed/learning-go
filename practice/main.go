648fda4de95c7ad3720e27859a97cbcb2136bb7b
ShamikhHameed <shamikhhameed@gmail.com>
2023-05-26 00:23:52 +0530

    getting input from the console

--- practice/main.go
+++ practice/main.go
@@ -1,40 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-const aConst int = 99
-
 func main() {
-	var aString string = "This is Go"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n\n", aString)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text : ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered : ", input)
 
-	var aInt int = 42
-	fmt.Println(aInt)
-	fmt.Printf("The variables type is %T\n\n", aInt)
-
-	var defaultInt int
-	fmt.Println(defaultInt)
-	fmt.Printf("The variables type is %T\n\n", defaultInt)
-
-	var anotherString = "This another string"
-	fmt.Println(anotherString)
-	fmt.Printf("The variables type is %T\n\n", anotherString)
-
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("The variables type is %T\n\n", anotherInt)
-
-	myString := "String with colon"
-	fmt.Println(myString)
-	fmt.Printf("The variables type is %T\n\n", myString)
-
-	myInt := 67
-	fmt.Println(myInt)
-	fmt.Printf("The variables type is %T\n\n", myInt)
-
-	fmt.Println(aConst)
-	fmt.Printf("The variables type is %T\n\n", aConst)
+	fmt.Print("Enter a number : ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Print("Value of number : ", aFloat)
+	}
 }
