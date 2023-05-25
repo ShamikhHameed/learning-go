c8942c3203c7e00e07c91ffb38ffc3e7c5a43a94
ShamikhHameed <shamikhhameed@gmail.com>
2023-05-26 01:32:39 +0530

    defining and calling functions

--- practice/main.go
+++ practice/main.go
@@ -5,42 +5,29 @@ import (
 )
 
 func main() {
-
-	colors := []string{"Red", "Green", "Blue"}
-	fmt.Println(colors)
-
-	for i := 0; i < len(colors); i++ {
-		fmt.Println(colors[i])
-	}
-
-	fmt.Printf("\n")
-
-	for i := range colors {
-		fmt.Println(colors[i])
-	}
-
-	fmt.Printf("\n")
-
-	for _, color := range colors {
-		fmt.Println(color)
-	}
-
-	fmt.Printf("\n")
-
-	value := 1
-	for value < 10 {
-		fmt.Println("Value:", value)
-	}
-
-	sum := 1
-	for sum < 1000 {
-		sum += sum
-		fmt.Println("Sum:", sum)
-		if sum > 300 {
-			goto theEnd
-		}
-	}
-
-	theEnd: fmt.Println("The end of program")
 	
+	doSomething()
+	sum := addValue(3, 5)
+	fmt.Println("The sum is:", sum)
+
+	multiSum, count := addMultiValue(3, 5, 6, 7)
+	fmt.Println("Multi sum value:", multiSum)
+	fmt.Println("Multi sum count:", count)
+}
+
+func doSomething() {
+	fmt.Println("This prints a text")
+}
+
+func addValue(val1, val2 int) int {
+	return val1 + val2
+}
+
+func addMultiValue(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
+
 }
