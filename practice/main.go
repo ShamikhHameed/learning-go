134915b04f83e0753a07d982d8d295e6e0de18cb
ShamikhHameed <shamikhhameed@gmail.com>
2023-05-26 01:17:10 +0530

    storing unordered values in maps

--- practice/main.go
+++ practice/main.go
@@ -6,30 +6,37 @@ import (
 )
 
 func main() {
+
+	states := make(map[string]string)
+	fmt.Println(states)
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	fmt.Println(states)
+
+	cali := states["CA"]
+	fmt.Println(cali)
+
+	delete(states, "OR")
+	states["NY"] = "New York"
+	fmt.Println(states)
+
+	for k, v := range states {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	fmt.Println(keys)
+	sort.Strings(keys)
+	fmt.Println(keys)
+
+	for i := range keys {
+		fmt.Println(states[keys[i]])
+	}
 	
-	var colors = []string{"Red", "Green", "Blue"}
-	fmt.Println(colors)
-	colors = append(colors, "Purple")
-	fmt.Println(colors)
-
-	colors = append(colors[1:len(colors)])
-	fmt.Println(colors)
-
-	colors = append(colors[:len(colors)-1])
-	fmt.Println(colors)
-
-	numbers := make([]int, 5)
-	numbers[0] = 89
-	numbers[1] = 34
-	numbers[2] = 53
-	numbers[3] = 83
-	numbers[4] = 93
-	fmt.Println(numbers)
-
-	numbers = append(numbers, 345)
-	fmt.Println(numbers)
-
-	sort.Ints(numbers)
-	fmt.Println(numbers)
-
 }
