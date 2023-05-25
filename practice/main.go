de47884dc86796ae2e3e0f4915557ed1c42b70c8
ShamikhHameed <shamikhhameed@gmail.com>
2023-05-26 01:14:07 +0530

    managing ordered values in slices

--- practice/main.go
+++ practice/main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	var colors [3]string
-	colors[0] = "Red"
-	colors[1] = "Green"
-	colors[2] = "Blue"
+	
+	var colors = []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
-	fmt.Println(colors[0])
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
 
-	var numbers = [5]int{5, 3, 1, 2, 4}
+	colors = append(colors[1:len(colors)])
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	numbers := make([]int, 5)
+	numbers[0] = 89
+	numbers[1] = 34
+	numbers[2] = 53
+	numbers[3] = 83
+	numbers[4] = 93
 	fmt.Println(numbers)
 
-	fmt.Println("Number of colors:", len(colors))
-	fmt.Println("Number of numbers:", len(numbers))
+	numbers = append(numbers, 345)
+	fmt.Println(numbers)
+
+	sort.Ints(numbers)
+	fmt.Println(numbers)
 
 }
