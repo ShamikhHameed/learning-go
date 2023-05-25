7f8e70f3bca2bbcc9ae24463e4c03030e8b21a97
ShamikhHameed <shamikhhameed@gmail.com>
2023-05-26 01:27:58 +0530

    creating loops with for statements

--- practice/main.go
+++ practice/main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
 
-	var result string
-	switch dow := rand.Intn(7) + 1; dow {
-	case 1:
-		result = "Sunday"
-	case 2:
-		result = "Monday"
-	default:
-		result = "Some day"
+	colors := []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
 	}
-	fmt.Println(result)
+
+	fmt.Printf("\n")
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	fmt.Printf("\n")
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	fmt.Printf("\n")
+
+	value := 1
+	for value < 10 {
+		fmt.Println("Value:", value)
+	}
+
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum:", sum)
+		if sum > 300 {
+			goto theEnd
+		}
+	}
+
+	theEnd: fmt.Println("The end of program")
+	
 }
